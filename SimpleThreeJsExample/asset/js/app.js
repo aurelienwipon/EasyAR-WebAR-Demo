b2dfcc59aa94039d4c6dd166c25482d969d747e6
jerry <smg@sightp.com>
2018-04-19 18:06:45 +0800

    增加three.js示例

---
+++ SimpleThreeJsExample/asset/js/app.js
@@ -0,0 +1,71 @@
+const webAR = new WebAR(1000, '/recognize.php');
+
+const threeHelper = new ThreeHelper();
+
+document.querySelector('#openCamera').addEventListener('click', function(){
+    const videoSetting = {width: 480, height: 360};
+
+    const video = document.querySelector('#video');
+    const videoDevice = document.querySelector('#videoDevice');
+
+    const openCamera = (video, deviceId, videoSetting) => {
+        webAR.openCamera(video, deviceId, videoSetting)
+            .then((msg) => {
+                // 打开摄像头成功
+                // 将视频铺满全屏(简单处理)
+                window.setTimeout(() => {
+                    let videoWidth = video.offsetWidth;
+                    let videoHeight = video.offsetHeight;
+
+                    if (window.innerWidth < window.innerHeight) {
+                        // 竖屏
+                        if (videoHeight < window.innerHeight) {
+                            video.setAttribute('height', window.innerHeight.toString() +'px');
+                        }
+                    }  else {
+                        // 横屏
+                        if (videoWidth < window.innerWidth) {
+                            video.setAttribute('width', window.innerWidth.toString() +'px');
+                        }
+                    }
+                }, 500);
+            })
+            .catch((err) => {
+                alert(err);
+                alert('打开视频设备失败');
+            });
+    };
+
+    // 列出视频设备
+    webAR.listCamera(videoDevice)
+        .then(() => {
+            openCamera(video, videoDevice.value, videoSetting);
+            videoDevice.onchange = () => {
+                openCamera(video, videoDevice.value, videoSetting);
+            };
+
+            document.querySelector('#openCamera').style.display = 'none';
+            document.querySelector('#start').style.display = 'inline-block';
+            document.querySelector('#stop').style.display = 'inline-block';
+        })
+        .catch((err) => {
+            console.info(err);
+            alert('没有可使用的视频设备');
+        });
+}, false);
+
+document.querySelector('#start').addEventListener('click', () => {
+    webAR.startRecognize((msg) => {
+        alert('识别成功');
+
+        // 识别成功后，从meta中取出model地址
+        const meta = JSON.parse(window.atob(msg.meta));
+        threeHelper.loadObject(meta.model);
+
+        webAR.trace('加载模型');
+    });
+}, false);
+
+document.querySelector('#stop').addEventListener('click', () => {
+    webAR.stopRecognize();
+}, false);
